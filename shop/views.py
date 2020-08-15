bcccf36c49b6b08439c59832330142987fbfff25
harshitag98 <harshitagrawal920@gmail.com>
2020-08-15 17:06:54 +0530

    multiple sliders according to categories

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    categories_products = Product.objects.values('category')
+    categories = {item['category'] for item in categories_products}
+    all_products = []
+    for cat in categories:
+        product = Product.objects.filter(category=cat)
+        n = len(product)
+        nSlides = n//4 + ceil((n/4) - (n//4))
+        all_products.append([product, range(1,nSlides+1), nSlides])
+    params = {'all_products': all_products}
+    return render(request, "shop/index.html", params)
 
 def about(request):
-    return HttpResponse("We are at about page")
+    return render(request, "shop/about.html")
 
 def contact(request):
     return HttpResponse("We are at contact page")
