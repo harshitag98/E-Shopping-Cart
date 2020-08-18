6f17648fd426dec7f7284f0c5ad5ee4a112e031a
harshitag98 <harshitagrawal920@gmail.com>
2020-08-18 20:22:32 +0530

    some changes

--- shop/static/shop/index.js
+++ shop/static/shop/index.js
@@ -1,3 +1,14 @@
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+// window.onscroll = function() {
+//     if(window.pageYOffset >= sticky){
+//         navbar.classList.add("sticky")
+//     }
+//     else{
+//         navbar.classList.remove("sticky");
+//     }
+// };
+
 var cart;
 
 if (localStorage.getItem('cart') == null) {
